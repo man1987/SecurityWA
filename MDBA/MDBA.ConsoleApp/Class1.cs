56cb06bafc1a3be13798e605d935f6b8cae9e840
Muhannad Assaf <muhannad.assaf@mdba.gov.au>
2026-01-14 09:50:14 +1100

    Adding Dynamics Packages

---
+++ MDBA/MDBA.ConsoleApp/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MDBA.ConsoleApp
+{
+
+
+    public class FewsPiRoot
+    {
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+
+        [JsonPropertyName("timeZone")]
+        public string TimeZone { get; set; }
+
+        [JsonPropertyName("timeSeries")]
+        public List<TimeSeriesItem> TimeSeries { get; set; }
+    }
+
+    public class TimeSeriesItem
+    {
+        [JsonPropertyName("header")]
+        public Header Header { get; set; }
+
+        [JsonPropertyName("events")]
+        public List<EventItem> Events { get; set; }
+    }
+
+    public class Header
+    {
+        public string Type { get; set; }
+        public string ModuleInstanceId { get; set; }
+        public string LocationId { get; set; }
+        public string ParameterId { get; set; }
+        public List<string> QualifierId { get; set; }
+        public TimeStep TimeStep { get; set; }
+        public DateHolder StartDate { get; set; }
+        public DateHolder EndDate { get; set; }
+        public string MissVal { get; set; }
+        public string StationName { get; set; }
+        public string Units { get; set; }
+
+        // Geographical coordinates
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+    }
+
+    public class TimeStep
+    {
+        public string Unit { get; set; }
+        public string Multiplier { get; set; }
+    }
+
+    public class DateHolder
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+
+    public class EventItem
+    {
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("time")]
+        public string Time { get; set; }
+
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+
+        [JsonPropertyName("flag")]
+        public string Flag { get; set; }
+    }
+}
--- MDBA/MDBA.ConsoleApp/Program.cs
+++ MDBA/MDBA.ConsoleApp/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MDBA.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main()
         {
+            try
+            {
+                var startDate = DateTime.Now.AddMonths(-2).ToString("yyyy-MM-ddTHH:mm:ssZ");
+                var endDate = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                string url = "http://mdbaprdrws01.prod.local:8080/FewsWebServices/rest/fewspiservice/v1/timeseries?filterId=Env_Watering_MDBA_Qenv&locationIds=R_401027&startTime=" + startDate + "&endTime=" + endDate + "&documentFormat=PI_JSON&documentVersion=1.34";
+                //string url = "http://mdbaprdrws01.prod.local:8080/FewsWebServices/rest/fewspiservice/v1/timeseries?filterId=Env_Watering_MDBA_Qenv&locationIds=R_401027&startTime=2024-12-01T08%3A00%3A00Z&endTime=2024-12-10T08%3A00%3A00Z&documentFormat=PI_JSON&documentVersion=1.34";
+
+                // Send the GET request
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                // Ensure we got a 2xx status code
+                response.EnsureSuccessStatusCode();
+
+                // Read the content as a string
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                FewsPiRoot data = JsonSerializer.Deserialize<FewsPiRoot>(responseBody, options);
+
+                
+
+                CreateHEWNSWRecords(data.TimeSeries.Where(ts => ts.Header.QualifierId.Contains("hew") && ts.Header.QualifierId.Contains("nsw")).FirstOrDefault());
+                //CreateHEWVICRecords();
+                //CreateRMIFNSWRecords();
+                //CreateRMIFVICRecords();
+                //CreateEWANSWRecords();
+                //CreateEWAVICRecords();
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+
+        private static void CreateHEWNSWRecords(TimeSeriesItem hewNSW)
+        {
+            hewNSW.Events.ForEach(day =>
+            {
+                var date = day.Date;
+                var state = "";
+                var qualifier = "";
+
+                
+            });
+            
+             
+
+
         }
     }
 }
